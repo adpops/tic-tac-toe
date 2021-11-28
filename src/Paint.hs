ff917393f67b0d44979960dbc38b68fac9d99a06
Jimmy <jaimin2468@gmail.com>
2021-11-28 00:12:57 -0800

    working on getting picture for X's and O's. xTile and oTile do not work

--- src/Paint.hs
+++ src/Paint.hs
@@ -16,16 +16,35 @@ getGridPath x = [ line [(x * cellWidth - 300, 0.0 + 300)
                        , (screenWidth/2, x * cellHeight - 300)
                        ]
                 ]       
-
 boardGrid = pictures (concatMap getGridPath [0..3])
 
-boardGameOn board = undefined
-boardGameOver winner board = undefined
+xTile x y = pictures [ rotate 45.0 $ rectangleSolid side 10.0
+                 , rotate (-45.0) $ rectangleSolid side 10.0
+                 ]
+    where side = min cellWidth cellHeight * 0.75
 
-winnerBoard = boardGrid
+oTile x y = thickCircle radius 10.0
+    where radius = min cellWidth cellHeight * 0.25
+
+getXPicture lst = undefined
+getYPicture lst = undefined
+
+findTileValues [] _ = []
+findTileValues board currPlayer = if (snd $ head board) == currPlayer then 
+                                     (fst (head board)):(findTileValues (tail board) currPlayer)
+                                  else
+                                      findTileValues (tail board) currPlayer
+
+boardGameOn :: [((Int, Int), Tile)] -> Picture
+boardGameOn board = pictures (xPic ++ oPic) where
+    xPic = getXPicture (findTileValues board (Just Player1))
+    oPic = getYPicture (findTileValues board (Just Player2))
+
+boardGameOver winner board = boardGrid
+
 
 worldToPicture :: World -> Picture
 worldToPicture world = 
     case state world of
         GameOn -> boardGameOn (board world)
-        GameOver winner -> winnerBoard    
+        GameOver winner -> boardGameOver winner board    
