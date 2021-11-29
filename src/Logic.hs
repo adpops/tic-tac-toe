1852bafb95e3246c8fa983834b678af54915abd1
Jimmy <jaimin2468@gmail.com>
2021-11-29 13:58:57 -0800

    changeBoard and change player seem to work

--- src/Logic.hs
+++ src/Logic.hs
@@ -6,43 +6,48 @@ import Debug.Trace
 --trace ("First val: " ++ (show (fst mousePos)) ++ " Second val: " ++ (show (snd mousePos)) ++ "\n")
 
 getTileX :: Float -> Board -> Int
+--getTileX _ [] = error "x coordinates of mouse click not found"
 getTileX _ [a] = fst (fst a)
 getTileX x board = if x >= (fromIntegral (-windowWidth))/2 + tileWidth * boardTileX && x < (fromIntegral (-windowWidth))/2 + tileWidth * (boardTileX + 1) then 
+                     trace ("x coordinates of mouse : " ++ (show $ fst (fst (head board))))
                      fst (fst (head board))
                  else 
                      getTileX x (tail board)
                  where boardTileX = fromIntegral (fst (fst (head board)))    
 
 getTileY :: Float -> Board -> Int
+--getTileY _ [] = error "y coordinates of mouse click not found"
 getTileY _ [a] = snd (fst a)
-getTileY y board = if y <= (fromIntegral windowHeight)/2 + tileHeight * boardTileY && y > (fromIntegral windowWidth)/2 + tileHeight * (boardTileY + 1) then
+getTileY y board = if y <= (fromIntegral windowHeight)/2 - tileHeight * boardTileY && y > (fromIntegral windowWidth)/2 - tileHeight * (boardTileY + 1) then
+                     trace ("y coordinates of mouse : " ++ (show $ snd (fst (head board))))
                      snd (fst (head board))
                  else
                      getTileY y (tail board)    
                  where boardTileY = fromIntegral (snd (fst (head board)))    
 
-getTile :: Int -> Int -> Board -> Tile
-getTile _ _ [a] = snd a
+getTile :: Int -> Int -> Board -> Board
+getTile _ _ [a] = [a]
 getTile x y board = if x == boardTileX && y == boardTileY then 
-                        snd (head board)
+                        trace ("Tile coordinates: " ++ (show (fst (head board))))
+                        [head board]
                     else 
                         getTile x y (tail board)
                     where
                         boardTileX = fst (fst (head board))
                         boardTileY = snd (fst (head board))
 
-getClickedTile :: Board -> (Float, Float) -> Tile
+getClickedTile :: Board -> (Float, Float) -> Board
 getClickedTile board pos = getTile (getTileX (fst pos) board) (getTileY (snd pos) board) board
 
-changeBoard :: Board -> Player -> (Float, Float) -> Board
+changeBoard :: Board -> Player -> Board -> Board
 changeBoard [] _ _ = []
-changeBoard board player pos = if (boardTileX, boardTileY) == fst (head board) then 
-                                   (fst (head board), (Just player)):(changeBoard (tail board) player pos)
+changeBoard board player tileInfo = if (boardTileX, boardTileY) == fst (head board) then 
+                                   (fst (head board), (Just player)):(changeBoard (tail board) player tileInfo)
                                else 
-                                   (head board):(changeBoard (tail board) player pos)
+                                   (head board):(changeBoard (tail board) player tileInfo)
                                where
-                                   boardTileX = getTileX (fst pos) board
-                                   boardTileY = getTileY (snd pos) board   
+                                   boardTileX = fst (fst (head tileInfo))
+                                   boardTileY = snd (fst (head tileInfo))  
 
 changePlayer :: Board -> Player -> Player
 changePlayer board player = if player == Player1 then Player2 else Player1
@@ -51,17 +56,17 @@ isGameOver :: Board -> Player -> GameState
 isGameOver board player = GameOn
 
 playGame :: World -> (Float, Float) -> World
-playGame world mousePos = if tile == Nothing then
-                              trace "Nothing Tile"  
+playGame world mousePos = if (snd (head currTileInfo)) == Nothing then
+                              trace "Nothing Tile" 
                               World 
-                              { board = changeBoard (board world) (player world) mousePos
+                              { board = changeBoard (board world) (player world) currTileInfo
                               , player = changePlayer (board world) (player world)
                               , state = isGameOver (board world) (player world)
                               }
                           else
                               trace "occupied tile"   
                               world
-                          where tile = getClickedTile (board world) mousePos
+                          where currTileInfo = getClickedTile (board world) mousePos
 
 inputEventHandler :: Event -> World -> World
 inputEventHandler (EventKey (MouseButton LeftButton) Up _ mousePos) world = 
