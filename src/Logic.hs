c66c1003b55ed0ebcd8aeb59dfd445fc98a14eaa
Jimmy <jaimin2468@gmail.com>
2021-11-28 18:59:02 -0800

    board is updating when clicked, however it is updating wrong tiles

--- src/Logic.hs
+++ src/Logic.hs
@@ -7,51 +7,61 @@ import Debug.Trace
 
 getTileX :: Float -> Board -> Int
 getTileX _ [a] = fst (fst a)
-getTileX x board = if x >= (fromIntegral (-windowWidth))/2 + tileWidth * boardX && x < (fromIntegral (-windowWidth))/2 + tileWidth * (boardX + 1) then 
+getTileX x board = if x >= (fromIntegral (-windowWidth))/2 + tileWidth * boardTileX && x < (fromIntegral (-windowWidth))/2 + tileWidth * (boardTileX + 1) then 
                      fst (fst (head board))
                  else 
                      getTileX x (tail board)
-                 where boardX = fromIntegral (fst (fst (head board)))    
+                 where boardTileX = fromIntegral (fst (fst (head board)))    
 
 getTileY :: Float -> Board -> Int
 getTileY _ [a] = snd (fst a)
-getTileY y board = if y <= (fromIntegral windowHeight)/2 + tileHeight * boardY && y > (fromIntegral windowWidth)/2 + tileHeight * (boardY + 1) then
+getTileY y board = if y <= (fromIntegral windowHeight)/2 + tileHeight * boardTileY && y > (fromIntegral windowWidth)/2 + tileHeight * (boardTileY + 1) then
                      snd (fst (head board))
                  else
                      getTileY y (tail board)    
-                 where boardY = fromIntegral (snd (fst (head board)))    
+                 where boardTileY = fromIntegral (snd (fst (head board)))    
 
 getTile :: Int -> Int -> Board -> Tile
 getTile _ _ [a] = snd a
-getTile x y board = if x == boardX && y == boardY then 
+getTile x y board = if x == boardTileX && y == boardTileY then 
                         snd (head board)
                     else 
                         getTile x y (tail board)
                     where
-                        boardX = fst (fst (head board))
-                        boardY = snd (fst (head board))
+                        boardTileX = fst (fst (head board))
+                        boardTileY = snd (fst (head board))
 
 getClickedTile :: Board -> (Float, Float) -> Tile
 getClickedTile board pos = getTile (getTileX (fst pos) board) (getTileY (snd pos) board) board
 
 changeBoard :: Board -> Player -> (Float, Float) -> Board
-changeBoard board player pos = board
+changeBoard [] _ _ = []
+changeBoard board player pos = if (boardTileX, boardTileY) == fst (head board) then 
+                                   (fst (head board), (Just player)):(changeBoard (tail board) player pos)
+                               else 
+                                   (head board):(changeBoard (tail board) player pos)
+                               where
+                                   boardTileX = getTileX (fst pos) board
+                                   boardTileY = getTileY (snd pos) board   
 
 changePlayer :: Board -> Player -> Player
-changePlayer board player = player
+changePlayer board player = if player == Player1 then Player2 else Player1
 
 isGameOver :: Board -> Player -> GameState
 isGameOver board player = GameOn
 
 playGame :: World -> (Float, Float) -> World
-playGame world mousePos = if getClickedTile (board world) mousePos == Nothing then
+playGame world mousePos = if tile == Nothing then
+                              trace "Nothing Tile"  
                               World 
                               { board = changeBoard (board world) (player world) mousePos
                               , player = changePlayer (board world) (player world)
                               , state = isGameOver (board world) (player world)
                               }
-                          else 
+                          else
+                              trace "occupied tile"   
                               world
+                          where tile = getClickedTile (board world) mousePos
 
 inputEventHandler :: Event -> World -> World
 inputEventHandler (EventKey (MouseButton LeftButton) Up _ mousePos) world = 
